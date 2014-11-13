d271887c14bcfe78f38e1627a7a161cae092b14b
Tony Aldridge <tony@dabapps.com>
2014-11-13 17:31:34 +0000

    Putting together dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+#![feature(phase)]
+
+#[phase(plugin, link)]
+extern crate conrod;
+extern crate event;
+extern crate sdl2_window;
+extern crate shader_version;
+
+use event::{Events, WindowSettings};
+use conrod::UiContext;
+use sdl2_window::Sdl2Window as Window;
+
 fn main() {
-    println!("Hello, world!")
+    let opengl = shader_version::opengl::OpenGL_3_2;
+    let window = Window::new(
+        opengl,
+        WindowSettings {
+            title: "SuperMegaTag".to_string(),
+            size: [800, 600],
+            fullscreen: false,
+            exit_on_esc: true,
+            samples: 4,
+        }
+    );
+    let context = UiContext::new(&Path::new("./assets/Dense-Regular.otf"), None).ok().expect("Couldn't get a graphics context!");
+
+    //let mut current_scene = connectscene::ConnectScene::new();
+    for ref e in Events::new(window) {
+        /*match current_scene.handle_event(e) {
+            Some(scene) => current_scene = scene,
+                None => ()
+        };*/
+    }
 }
