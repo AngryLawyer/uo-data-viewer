460f7dd953f030dbc5d740d09d34e16cf22c56bf
Tony Aldridge <zaragopha@hotmail.com>
2014-11-14 21:09:17 +0000

    Skills are now output correctly

--- src/skillsscene.rs
+++ src/skillsscene.rs
@@ -30,6 +30,18 @@ impl SkillsScene {
         uic.background().color(Color::black()).draw(gl);
         match self.skills {
             Ok(ref skills) => {
+                for (idx, skill) in skills.skills.iter().enumerate() {
+                    let glyph = if skill.clickable {
+                        "+"
+                    } else {
+                        "-"
+                    };
+                    uic.label(format!("{} {}", glyph, skill.name).as_slice())
+                        .position(((idx / 20) * 256) as f64, ((idx % 20) * 16) as f64)
+                        .size(16u32)
+                        .color(Color::white())
+                        .draw(gl);
+                }
             },
             Err(ref error) => {
                 uic.label(format!("{}", error).as_slice())
