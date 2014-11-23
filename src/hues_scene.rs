041905f718c87784c411f009a646999fe00ce097
Tony Aldridge <zaragopha@hotmail.com>
2014-11-23 12:59:38 +0000

    Updated to latest

--- src/hues_scene.rs
+++ src/hues_scene.rs
@@ -82,7 +82,7 @@ impl HuesScene {
         let c = Context::abs(args.width as f64, args.height as f64);
 
         for (col_idx, &color) in hue.color_table.iter().enumerate() {
-            let (r, g, b) = color.to_tuple();
+            let (r, g, b, _) = color.to_rgba();
             c.rect((256.0 + 128.0) + (col_idx * 16) as f64, (32 + (index * 16)) as f64, 16.0, 16.0)
             .rgb(r as f32 / 255.0, g as f32 / 255.0, b as f32 / 255.0)
             .draw(gl);
