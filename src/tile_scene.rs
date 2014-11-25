899d4aded9a448a68b716b5412e57599bb3ac904
Tony Aldridge <zaragopha@hotmail.com>
2014-11-25 22:05:22 +0000

    Pull through multiple tiles

--- src/tile_scene.rs
+++ src/tile_scene.rs
@@ -20,65 +20,54 @@ use image::{GenericImage, ImageBuf, Rgba};
 
 use std::io::IoResult;
 
+fn create_slice(reader: &mut ArtReader) -> Texture {
+    let max_x = 18;
+    let max_y = 13;
+    let limit = max_x * max_y;
+    let idx = 0;
+    let start = limit * idx;
+    let mut dest = ImageBuf::new(800, 600);
+
+    for x in range(0, max_x) {
+        for y in range(0, max_y) {
+            let maybe_tile = reader.read((x % max_y) + (y * max_x));
+            match maybe_tile {
+                Ok(TileOrStatic::Tile(tile)) => {
+                    let (width, height, data) = tile.to_32bit();
+                    let buf = ImageBuf::from_fn(width, height, |x, y| {
+                        let (r, g, b, a) = data[((x % height) + (y * width)) as uint].to_rgba();
+                        Rgba(r, g, b, a)
+                    });
+                    dest.put_image(44 * x, 44 * y, &buf)
+                },
+                _ => ()
+            }
+        }
+    }
+
+    Texture::from_image(&dest)
+}
+
 pub struct TileScene {
     reader: IoResult<ArtReader>,
-    image: ImageBuf<Rgba<u8>>,
     texture: Texture
 }
 
 impl TileScene {
     pub fn new() -> BoxedScene {
         let mut reader = ArtReader::new(&Path::new("./assets/artidx.mul"), &Path::new("./assets/art.mul"));
-        let mut image = match reader {
-            Ok(ref mut reader) => {
-                let tile = match reader.read(0).ok().expect("Couldn't load tile 0") {
-                    TileOrStatic::Tile(tile) => tile,
-                    TileOrStatic::Static(tile) => panic!("LOL")
-                };
-                let (width, height, data) = tile.to_32bit();
-                ImageBuf::from_fn(width, height, |x, y| {
-                    let (r, g, b, a) = data[((x % height) + (y * width)) as uint].to_rgba();
-                    Rgba(r, g, b, a)
-                })
-            }
-            _ => ImageBuf::new(44, 44)
+        let texture = match reader {
+            Ok(ref mut reader) => create_slice(reader),
+            _ => panic!("Oh dear")
         };
-        let mut texture = Texture::from_image(&image);
-
         let mut scene = box TileScene {
             reader: reader,
-            image: image,
             texture: texture
         };
 
         scene
     }
 
-    fn create_slice(&mut self, reader: &mut ArtReader) {
-        let max_x = 18;
-        let max_y = 13;
-        let limit = max_x * max_y;
-        let idx = 0;
-        let start = limit * idx;
-
-        for x in range(0, max_x) {
-            for y in range(0, max_y) {
-                let maybe_tile = reader.read((x % max_y) + (y * max_x));
-                match maybe_tile {
-                    Ok(TileOrStatic::Tile(tile)) => {
-                        let (width, height, data) = tile.to_32bit();
-                        let buf = ImageBuf::from_fn(width, height, |x, y| {
-                            let (r, g, b, a) = data[((x % height) + (y * width)) as uint].to_rgba();
-                            Rgba(r, g, b, a)
-                        });
-                        let texture = Texture::new(0, 44, 44);
-                    },
-                    _ => ()
-                }
-            }
-        }
-
-    }
 
     fn render(&self, args: RenderArgs, uic: &mut UiContext, gl: &mut Gl) {
         uic.background().color(Color::black()).draw(gl);
