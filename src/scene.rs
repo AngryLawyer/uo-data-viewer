4702a715d92091c8a94be4ca4e4a244d40b7eb29
Tony Aldridge <tony@dabapps.com>
2016-11-09 14:07:07 +0000

    Started building a framework for stacks of scenes

--- src/scene.rs
+++ src/scene.rs
@@ -6,3 +6,36 @@ pub trait Scene {
     fn render(&self, renderer: &mut Renderer);
     fn handle_event(&self, event: &Event);
 }
+
+pub struct SceneStack {
+    scenes: Vec<BoxedScene>
+}
+
+impl SceneStack {
+
+    pub fn new() -> SceneStack {
+        SceneStack {
+            scenes: vec![]
+        }
+    }
+
+    pub fn push_scene(&mut self, scene: BoxedScene) {
+        self.scenes.push(scene)
+    }
+
+    pub fn swap_scene(&mut self, scene: BoxedScene) -> Option<BoxedScene> {
+        let old_scene = self.scenes.pop();
+        self.scenes.push(scene);
+        old_scene
+    }
+
+    pub fn pop_scene(&mut self) -> Option<BoxedScene> {
+        self.scenes.pop()
+    }
+
+    pub fn render(&self, renderer: &mut Renderer) {
+    }
+
+    pub fn handle_event(&self, event: &Event) {
+    }
+}
