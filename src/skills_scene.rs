45a71ff5a9ebbf8649cc631adf894cfd2498ee64
Tony Aldridge <zaragopha@hotmail.com>
2016-11-12 19:33:38 +0000

    src/[A

--- src/skills_scene.rs
+++ src/skills_scene.rs
@@ -12,7 +12,7 @@ use sdl2::keyboard::Keycode;
 use sdl2_ttf::Font;
 
 pub struct SkillsScene {
-    text: Texture
+    pages: Vec<Texture>,
 }
 
 impl SkillsScene {
@@ -20,60 +20,41 @@ impl SkillsScene {
         let skills = Skills::new(&Path::new("./assets/skills.idx"), &Path::new("./assets/skills.mul"));
         let text = match skills {
             Ok(skills) => {
-                let items: Vec<String> = skills.skills.iter().map(|skill| {
-                    let glyph = if skill.clickable {
-                        "+"
-                    } else {
-                        "-"
-                    };
-                    format!("{} {}", glyph, skill.name)
+                let items: Vec<Texture> = skills.skills.chunks(30).map(|chunk| {
+                    let skills: Vec<String> = chunk.iter().map(|skill| {
+                        let glyph = if skill.clickable {
+                            "+"
+                        } else {
+                            "-"
+                        };
+                        format!("{} {}", glyph, skill.name)
+                    }).collect();
+                    text_renderer.create_text(renderer, &skills.join("\n"), Color::RGBA(255, 255, 255, 255))
                 }).collect();
-                items.join("\n")
+                items
             },
             Err(error) => {
-                format!("{}", error)
+                let text = format!("{}", error);
+                let texture = text_renderer.create_text(renderer, &text, Color::RGBA(255, 255, 255, 255));
+                vec![texture]
             }
         };
         Box::new(SkillsScene {
-            text: text_renderer.create_text(renderer, &text, Color::RGBA(255, 255, 255, 255))
+            pages: text,
         })
     }
-
-    /*fn render(&mut self, _args: RenderArgs, uic: &mut UiContext, gl: &mut Gl) {
-        gl.enable_alpha_blend();
-        uic.background().color(Color::black()).draw(gl);
-        match self.skills {
-            Ok(ref skills) => {
-                for (idx, skill) in skills.skills.iter().enumerate() {
-                    let glyph = if skill.clickable {
-                        "+"
-                    } else {
-                        "-"
-                    };
-                    uic.label(format!("{} {}", glyph, skill.name).as_slice())
-                        .position(((idx / 20) * 256) as f64, ((idx % 20) * 16) as f64)
-                        .size(16u32)
-                        .color(Color::white())
-                        .draw(gl);
-                }
-            },
-            Err(ref error) => {
-                uic.label(format!("{}", error).as_slice())
-                    .position(0.0, 0.0)
-                    .size(16u32)
-                    .color(Color::white())
-                    .draw(gl);
-            }
-        }
-    }*/
 }
 
 impl<T> Scene<T> for SkillsScene {
     fn render(&self, renderer: &mut Renderer) {
-        let TextureQuery {width, height, .. } = self.text.query();
-        let target = Rect::new(0, 0, width, height);
         renderer.clear();
-        renderer.copy(&self.text, None, Some(target)).unwrap();
+        let mut last_width = 0;
+        for page in self.pages.iter() {
+            let TextureQuery {width, height, .. } = page.query();
+            let target = Rect::new(last_width, 0, width, height);
+            renderer.copy(page, None, Some(target)).unwrap();
+            last_width += width;
+        }
         renderer.present();
     }
 
