7ed900b39670be197bc457cb6a645111e968f2fa
Tony Aldridge <zaragopha@hotmail.com>
2016-12-09 22:23:55 +0000

    Avoid hitting the file system as much as before

--- src/map_scene.rs
+++ src/map_scene.rs
@@ -50,9 +50,21 @@ impl MapLens {
     pub fn update(&self, map_reader: &mut MapReader, x: u32, y: u32) -> MapLens {
         let difference_x = x as i64 - self.x as i64;
         let difference_y = y as i64 - self.y as i64;
-        println!("{} {}", difference_x, difference_y);
+        let mut blocks = vec![];
+        for yy in 0..self.height {
+            for xx in 0..self.width {
+                let offset_x = xx as i64 + difference_x;
+                let offset_y = yy as i64 + difference_y;
+                if offset_x < 0 || offset_y < 0 || offset_x >= self.width as i64 || offset_y >= self.height as i64 {
+                    blocks.push(map_reader.read_block_from_coordinates(x + xx, y + yy).ok());
+                } else {
+                    blocks.push(self.blocks[(offset_x + (offset_y * self.width as i64)) as usize]);
+                }
+            }
+        }
+        
         MapLens {
-            blocks: self.blocks.clone(),
+            blocks: blocks,
             x: x,
             y: y,
             width: self.width,
