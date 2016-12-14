cc21527bf806fdc09329593434b655fbc8c8d372
Tony Aldridge <tony@dabapps.com>
2016-12-14 13:35:36 +0000

    Provide type information for HuesScene

--- src/hues_scene.rs
+++ src/hues_scene.rs
@@ -9,14 +9,15 @@ use uorustlibs::art::{ArtReader, Art};
 use uorustlibs::hues::{HueReader, HueGroup, Hue};
 use uorustlibs::color::Color as ColorTrait;
 
-use std::io::{Error, };
+use std::io::{Error};
+use std::fs::{File};
 use sdl2::render::{Renderer, Texture, TextureQuery};
 use sdl2::rect::Rect;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
 pub struct HuesScene {
-    reader: Result<HueReader>,
+    reader: Result<HueReader<File>>,
     index: u32,
     texture: Option<Texture>
 }
