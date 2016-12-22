41869430f0899f2f105084435e28e301febea946
Tony Aldridge <tony@dabapps.com>
2016-12-22 17:25:07 +0000

    Easier to read errors for gumps

--- src/gump_scene.rs
+++ src/gump_scene.rs
@@ -34,46 +34,30 @@ impl GumpScene {
     }
 
     fn create_slice(&mut self, renderer: &mut Renderer, engine_data: &mut EngineData) {
+        let mut dest = Surface::new(1024, 768, PixelFormatEnum::RGBA8888).unwrap();
         match self.reader {
             Ok(ref mut reader) => {
                 match reader.read_gump(self.index) {
                     Ok(gump) => {
-                        let mut dest = Surface::new(1024, 768, PixelFormatEnum::RGBA8888).unwrap();
                         dest.fill_rect(None, Color::RGB(0, 0, 0)).unwrap();
                         let surface = gump.to_surface();
                         surface.blit(None, &mut dest, Some(Rect::new(0, 0, surface.width(), surface.height())));
                         let label = engine_data.text_renderer.create_text(&format!("{}", self.index), Color::RGBA(255, 255, 255, 255));
                         label.blit(None, &mut dest, Some(Rect::new(9, surface.height() as i32 + 16, label.width(), label.height())));
-                        self.texture = Some(renderer.create_texture_from_surface(&dest).unwrap());
                     },
                     _ => {
-                        let texture = engine_data.text_renderer.create_text_texture(renderer, "Could not create gump", Color::RGBA(255, 255, 255, 255));
-                        self.texture = Some(texture);
+                        let error = engine_data.text_renderer.create_text("Invalid gump", Color::RGBA(255, 255, 255, 255));
+                        error.blit(None, &mut dest, Some(Rect::new(0, 0, error.width(), error.height())));
+                        let label = engine_data.text_renderer.create_text(&format!("{}", self.index), Color::RGBA(255, 255, 255, 255));
+                        label.blit(None, &mut dest, Some(Rect::new(0, 16, label.width(), label.height())));
                     }
                 }
-
-                /*for y in 0..MAX_Y {
-                    for x in 0..MAX_X {
-                        let maybe_tile = reader.read_tile(start + x + (y * MAX_X));
-                        let index = start + x + (y * MAX_X);
-                        match maybe_tile {
-                            Ok(tile) => {
-                                let surface = tile.to_surface();
-                                surface.blit(None, &mut dest, Some(Rect::new(44 * x as i32, (44 + 16) * y as i32, 44, 44)));
-                            },
-                            _ => ()
-                        };
-                        let label = engine_data.text_renderer.create_text(&format!("{}", index), Color::RGBA(255, 255, 255, 255));
-                        label.blit(None, &mut dest, Some(Rect::new(44 * x as i32, ((44 + 16) * y as i32) + 44, label.width(), label.height())));
-                    }
-                }*/
-
             },
             _ => {
-                let texture = engine_data.text_renderer.create_text_texture(renderer, "Could not create slice", Color::RGBA(255, 255, 255, 255));
-                self.texture = Some(texture);
+                let error = engine_data.text_renderer.create_text("Could not create slice", Color::RGBA(255, 255, 255, 255));
             }
         }
+        self.texture = Some(renderer.create_texture_from_surface(&dest).unwrap());
     }
 }
 
