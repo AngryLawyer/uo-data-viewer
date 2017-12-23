9ea93c0904654fb33492372efa2e7c2f6e0c40b4
Tony Aldridge <zaragopha@hotmail.com>
2017-12-23 12:43:41 +0000

    Correct implementation of Image to Surface

--- src/image_convert.rs
+++ src/image_convert.rs
@@ -1,11 +1,18 @@
-use image::RgbaImage;
+use image::{RgbaImage, Rgba};
 use sdl2::surface::Surface;
 use sdl2::pixels::{PixelFormatEnum};
 
 pub fn image_to_surface(image: &RgbaImage) -> Surface {
     let mut surface = Surface::new(image.width(), image.height(), PixelFormatEnum::RGBA8888).expect("Failed to create surface");
+    let mut idx = 0;
+
     surface.with_lock_mut(|data| {
-        data.copy_from_slice(&image.to_vec());
+        let mut copied = image.clone().into_raw();
+        // TODO: This appears to be endian-specific, and should be sorted
+        for chunk in copied.chunks_mut(4) {
+            chunk.reverse();
+        }
+        data.copy_from_slice(&copied);
     });
     surface
 }
